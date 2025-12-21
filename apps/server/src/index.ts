ec449459a6a946ff5f9f229fdc7759d00ec19c81
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-12-21 16:26:50 +0000

    feat: init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import { google } from "@ai-sdk/google";
+import { trpcServer } from "@hono/trpc-server";
+import { createContext } from "@my-better-t-app/api/context";
+import { appRouter } from "@my-better-t-app/api/routers/index";
+import { auth } from "@my-better-t-app/auth";
+import { streamText, convertToModelMessages } from "ai";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: process.env.CORS_ORIGIN || "",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  }),
+);
+
+app.post("/ai", async (c) => {
+  const body = await c.req.json();
+  const uiMessages = body.messages || [];
+  const result = streamText({
+    model: google("gemini-2.5-flash"),
+    messages: convertToModelMessages(uiMessages),
+  });
+
+  return result.toUIMessageStreamResponse();
+});
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
