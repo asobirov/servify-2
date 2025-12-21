f2dafa58e0bfde2d7f7697998b83ec8ef9167579
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-12-21 21:11:03 +0000

    feat: add initial Expo configuration and EAS build settings

---
+++ apps/native/app.config.ts
@@ -0,0 +1,69 @@
+import type { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  version: "0.1.0",
+  name: "servify",
+  slug: "servify",
+  scheme: "servify",
+  owner: "asobirov",
+  orientation: "portrait",
+  icon: "./assets/icon-light.png",
+  userInterfaceStyle: "automatic",
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  newArchEnabled: true,
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    bundleIdentifier: "uz.servify.app",
+    supportsTablet: true,
+    icon: {
+      light: "./assets/icon.png",
+      dark: "./assets/icon.png",
+    },
+    usesAppleSignIn: true,
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+      NSAllowsArbitraryLoads:
+        process.env.NODE_ENV === "development" || undefined,
+    },
+    associatedDomains: ["webcredentials:servify.uz"],
+  },
+  android: {
+    package: "uz.servify.app",
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#1F104A",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  extra: {
+    eas: {
+      projectId: "de142bdc-0615-439e-a3f8-cf33f3274893",
+    },
+  },
+  experiments: {
+    reactServerFunctions: true,
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+  plugins: [
+    "expo-router",
+    "expo-secure-store",
+    "expo-localization",
+    "expo-font",
+    "react-native-maps",
+    [
+      "expo-splash-screen",
+      {
+        backgroundColor: "#09090b",
+        image: "./assets/splash-icon.png",
+        dark: {
+          backgroundColor: "#ffffff",
+          image: "./assets/splash-icon.png",
+        },
+      },
+    ],
+  ],
+});
