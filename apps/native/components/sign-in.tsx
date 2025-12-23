74b57f85b515b335eeaea6c4bade233dc17bff38
Laroikin <said24abdi04@gmail.com>
2025-12-23 21:15:23 +0500

    refactor: enhance sign-in component by extracting Telegram authentication logic into a callback function and update configuration schema

--- apps/native/components/sign-in.tsx
+++ apps/native/components/sign-in.tsx
@@ -1,5 +1,4 @@
-import { Card, useThemeColor, Button } from "heroui-native";
-import { Text, Pressable } from "react-native";
+import { Card, Button } from "heroui-native";
 import { Ionicons } from "@expo/vector-icons";
 import { withUniwind } from "uniwind";
 import * as WebBrowser from "expo-web-browser";
@@ -7,12 +6,25 @@ import * as WebBrowser from "expo-web-browser";
 const StyledIonicons = withUniwind(Ionicons);
 
 import { authClient } from "@/lib/auth-client";
+import { useCallback } from "react";
 
 function SignIn() {
-  const mutedColor = useThemeColor("muted");
-  const accentColor = useThemeColor("accent");
-  const foregroundColor = useThemeColor("foreground");
-  const dangerColor = useThemeColor("danger");
+  const telegramSignIn = useCallback(async () => {
+    const result = await WebBrowser.openAuthSessionAsync(
+      "https://oauth.telegram.org/auth?bot_id=8361741979&origin=https://ready-geckos-write.loca.lt/telegram&embed=1&request_access=write&return_to=https://ready-geckos-write.loca.lt/telegram",
+      "servify://",
+    );
+
+    if (result.type !== "success") {
+      return;
+    }
+
+    const resultURL = new URL(result.url);
+
+    const user = JSON.parse(atob(resultURL.searchParams.get("tgAuthResult") || "") || "{}");
+
+    await authClient.signIn.telegram(user as any);
+  }, []);
 
   return (
     <>
@@ -36,22 +48,7 @@ function SignIn() {
         <Button
           variant="secondary"
           className="flex flex-row justify-between"
-          onPress={async () => {
-            const result = await WebBrowser.openAuthSessionAsync(
-              "https://oauth.telegram.org/auth?bot_id=8361741979&origin=https://ready-geckos-write.loca.lt/telegram&embed=1&request_access=write&return_to=https://ready-geckos-write.loca.lt/telegram",
-              "servify://",
-            );
-
-            if (result.type !== "success") {
-              return;
-            }
-
-            const resultURL = new URL(result.url);
-
-            const user = JSON.parse(atob(resultURL.searchParams.get("tgAuthResult") || "") || "{}");
-
-            await authClient.signIn.telegram(user as any);
-          }}
+          onPress={telegramSignIn}
         >
           <StyledIonicons name="paper-plane" size={16} className="-mr-4 text-foreground" />
           <Button.Label className="self-center mx-auto text-foreground">
