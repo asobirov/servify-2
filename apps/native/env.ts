7f848c9f3b47710ebdc262b703c048c6c4e926bb
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-12-23 15:38:51 +0000

    feat: add native env validation

---
+++ apps/native/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+  },
+
+  clientPrefix: "EXPO_PUBLIC",
+  client: {},
+
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+});
