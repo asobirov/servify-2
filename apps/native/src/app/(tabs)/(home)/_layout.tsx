1a78af9c8e0fa8d9a9d8ff2091f28ded70a0c1bb
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-12-23 23:53:42 +0000

    chore: update dependencies and improve layout components with new animations and navigation links

--- apps/native/src/app/(tabs)/(home)/_layout.tsx
+++ apps/native/src/app/(tabs)/(home)/_layout.tsx
@@ -1,11 +1,18 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Trans, useLingui } from "@lingui/react/macro";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { useThemeColor } from "heroui-native";
+import { Pressable } from "react-native";
+import Animated from "react-native-reanimated";
+import { withUniwind } from "uniwind";
 
 import { ThemeToggle } from "@/components/theme-toggle";
 
+const StyledIonicons = withUniwind(Ionicons);
+
 export default function HomeLayout() {
   const themeColorForeground = useThemeColor("foreground");
+  const themeColorBackground = useThemeColor("background");
 
   const { t } = useLingui();
 
@@ -30,6 +37,15 @@ export default function HomeLayout() {
           headerTitle: t`Home`,
           //   headerLargeTitle: true,
           headerRight: () => <ThemeToggle />,
+          headerLeft: () => (
+            <Link href="/(tabs)/(home)/modal" asChild>
+              <Pressable className="flex items-center justify-center">
+                <Animated.View key="modal" className={"items-center justify-center"}>
+                  <StyledIonicons name="information-circle-outline" size={20} className="text-foreground" />
+                </Animated.View>
+              </Pressable>
+            </Link>
+          ),
         }}
       />
     </Stack>
---
+++ apps/native/src/app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { Container } from "@/components/container";
+
+export default function Search() {
+  return (
+    <Container>
+      <Text className="text-foreground">Search</Text>
+    </Container>
+  );
+}
