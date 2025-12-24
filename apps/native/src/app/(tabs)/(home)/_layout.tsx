f05520d432ccbec5d4cf060b0ab572b8a1a2ead4
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-12-24 10:40:53 +0000

    feat: add custom Stack

--- apps/native/src/app/(tabs)/(home)/_layout.tsx
+++ apps/native/src/app/(tabs)/(home)/_layout.tsx
@@ -1,36 +1,20 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Trans, useLingui } from "@lingui/react/macro";
-import { Link, Stack } from "expo-router";
-import { useThemeColor } from "heroui-native";
+import { useLingui } from "@lingui/react/macro";
+import { Link } from "expo-router";
 import { Pressable } from "react-native";
 import Animated from "react-native-reanimated";
 import { withUniwind } from "uniwind";
 
+import { Stack } from "@/components/stack";
 import { ThemeToggle } from "@/components/theme-toggle";
 
 const StyledIonicons = withUniwind(Ionicons);
 
 export default function HomeLayout() {
-  const themeColorForeground = useThemeColor("foreground");
-  const themeColorBackground = useThemeColor("background");
-
   const { t } = useLingui();
 
   return (
-    <Stack
-      screenOptions={{
-        headerTransparent: true,
-        headerShadowVisible: true,
-        headerLargeTitleShadowVisible: false,
-        headerLargeStyle: {
-          backgroundColor: "transparent",
-        },
-        headerLargeTitleStyle: { color: themeColorForeground },
-
-        headerTintColor: themeColorForeground,
-        headerTitleStyle: { color: themeColorForeground },
-      }}
-    >
+    <Stack>
       <Stack.Screen
         name="index"
         options={{
@@ -41,7 +25,11 @@ export default function HomeLayout() {
             <Link href="/(tabs)/(home)/modal" asChild>
               <Pressable className="flex items-center justify-center">
                 <Animated.View key="modal" className={"items-center justify-center"}>
-                  <StyledIonicons name="information-circle-outline" size={20} className="text-foreground" />
+                  <StyledIonicons
+                    name="information-circle-outline"
+                    size={20}
+                    className="text-foreground"
+                  />
                 </Animated.View>
               </Pressable>
             </Link>
---
+++ apps/native/src/components/stack.tsx
@@ -0,0 +1,124 @@
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+import { isLiquidGlassAvailable } from "expo-glass-effect";
+import { Stack as ExpoStack, useRouter } from "expo-router";
+import { useThemeColor } from "heroui-native";
+import React from "react";
+import { Pressable, View } from "react-native";
+import { withUniwind } from "uniwind";
+
+const DEFAULT_STACK_HEADER = ({
+  headerTextColor,
+  backgroundColor,
+  isLiquidGlassAvailable,
+}: {
+  headerTextColor: string;
+  backgroundColor?: string;
+  isLiquidGlassAvailable: boolean;
+}): NativeStackNavigationOptions =>
+  process.env.EXPO_OS !== "ios"
+    ? {}
+    : {
+        contentStyle: {
+          backgroundColor: backgroundColor,
+        },
+        headerTransparent: true,
+        headerBlurEffect: isLiquidGlassAvailable ? undefined : "systemChromeMaterial",
+        headerShadowVisible: true,
+        headerLargeTitleShadowVisible: false,
+        headerLargeStyle: {
+          backgroundColor: "transparent",
+        },
+        headerLargeTitle: true,
+        headerTintColor: headerTextColor,
+        // headerTitleStyle: {
+        // //   color: "",
+        // },
+        headerLargeTitleStyle: { color: headerTextColor },
+      };
+
+/** Create a bottom sheet on iOS with extra snap points (`sheetAllowedDetents`) */
+export const BOTTOM_SHEET: NativeStackNavigationOptions = {
+  // https://github.com/software-mansion/react-native-screens/blob/main/native-stack/README.md#sheetalloweddetents
+  presentation: "formSheet",
+  gestureDirection: "vertical",
+  animation: "slide_from_bottom",
+  sheetGrabberVisible: true,
+  sheetInitialDetentIndex: 0,
+  sheetAllowedDetents: [0.5, 1.0],
+};
+
+const StyledIonicons = withUniwind(Ionicons);
+
+export function Stack({
+  screenOptions,
+  children,
+  ...props
+}: React.ComponentProps<typeof ExpoStack>) {
+  const router = useRouter();
+
+  const headerTextColor = useThemeColor("foreground");
+  const backgroundColor = useThemeColor("background");
+
+  const processedChildren = React.Children.map(children, (child: React.ReactNode) => {
+    console.log("isLiquidGlassAvailable", isLiquidGlassAvailable());
+    if (!React.isValidElement<StackScreenProps>(child)) {
+      return child;
+    }
+
+    const { sheet, modal, ...props } = child.props;
+    if (sheet) {
+      return React.cloneElement(child, {
+        ...props,
+        options: {
+          ...BOTTOM_SHEET,
+          ...props.options,
+        },
+      });
+    } else if (modal) {
+      return React.cloneElement(child, {
+        ...props,
+        options: {
+          presentation: "modal",
+          headerLargeTitle: false,
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <View className="flex items-center justify-center">
+                <StyledIonicons name="close-circle" size={20} className="text-foreground" />
+              </View>
+            </Pressable>
+          ),
+          ...props.options,
+        },
+      });
+    }
+
+    return child;
+  });
+
+  return (
+    <ExpoStack
+      screenOptions={{
+        ...DEFAULT_STACK_HEADER({
+          isLiquidGlassAvailable: isLiquidGlassAvailable(),
+          headerTextColor: `hsl(${headerTextColor})`,
+          backgroundColor: `hsl(${backgroundColor})`,
+        }),
+        ...screenOptions,
+      }}
+      {...props}
+      children={processedChildren}
+    />
+  );
+}
+
+type StackScreenProps = React.ComponentProps<typeof ExpoStack.Screen> & {
+  /** Make the sheet open as a bottom sheet with default options on iOS. */
+  sheet?: boolean;
+  /** Make the screen open as a modal. */
+  modal?: boolean;
+};
+
+Stack.Screen = ExpoStack.Screen as React.FC<StackScreenProps>;
+
+Stack.Protected = ExpoStack.Protected;
