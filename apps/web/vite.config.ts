5fd3dd216ce172fb0ffd8c9e88420333bee1d98f
Laroikin <said24abdi04@gmail.com>
2025-12-24 17:12:54 +0500

    feat: add nitro integration to Vite config, update TypeScript configuration with output directory and project references, and include new dependencies in package.json

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsConfigPaths(), tailwindcss(), tanstackStart(), react()],
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      preset: "bun",
+    }),
+    react(),
+  ],
   server: {
     port: 3001,
   },
